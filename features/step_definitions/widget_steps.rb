25fc0705c4d4bd61d4e8711ab8bb24a11ac6987f
Rich Downie <rich@blackboxd.com>
2018-12-31 09:04:09 -0500

    updating env.rb

--- features/step_definitions/widget_steps.rb
+++ features/step_definitions/widget_steps.rb
@@ -7,8 +7,7 @@ Given /I measure the performance of the widgets page "(.*)" times/i do |number|
     @widgets_index.visit
     load_secs = @browser.performance.summary[:response_time].to_f / 1000
     dom_processing = @browser.performance.summary[:dom_processing].to_f / 1000
-    puts "Load Time: #{sprintf "%.2f", load_secs} seconds."
-    puts "Dom Processing Time: #{sprintf "%.2f", dom_processing} seconds."
-    # puts @browser.performance
+    puts "#{ENV["BROWSER"].upcase} Load Time: #{sprintf "%.2f", load_secs} seconds."
+    puts "#{ENV["BROWSER"].upcase} Dom Processing Time: #{sprintf "%.2f", dom_processing} seconds."
   end
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,12 +1,10 @@
 Before do
-  @browser = Watir::Browser.new #Chrome is the default
-  #You can try these other browsers
-  # @browser = Watir::Browser.new :firefox
-  # @browser = Watir::Browser.new :internet_explorer
-  # @browser = Watir::Browser.new :edge
-
-    @domain = 'http://localhost:3000'
-      # @domain = 'https://fullstackautomationwithruby.com'
+  if ENV["BROWSER"]
+    @browser = Watir::Browser.new ENV["BROWSER"].to_sym
+  else
+    @browser = Watir::Browser.new #Chrome is the default
+  end
+  @domain = 'https://fullstackautomationwithruby.com' || ENV["DOMAIN"]
 end
 
 After do
