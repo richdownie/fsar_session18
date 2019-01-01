4620a56e397f8176b9dbea466e6a8e6672045abd
Rich Downie <rich@blackboxd.com>
2019-01-01 10:40:28 -0500

    updates

--- features/step_definitions/widget_steps.rb
+++ features/step_definitions/widget_steps.rb
@@ -1,13 +1,9 @@
-Given /I am on the fsar widgets page/ do
-  @widgets_index.visit
-end
-
 Given /I measure the performance of the widgets index page "(.*)" times/i do |number|
   number.to_i.times do
     @widgets_index.visit
-    load_secs = @browser.performance.summary[:response_time].to_f / 1000
-    dom_processing = @browser.performance.summary[:dom_processing].to_f / 1000
-    puts "#{ENV["BROWSER"]} Repsonse Time: #{sprintf "%.2f", load_secs} seconds."
-    puts "#{ENV["BROWSER"]} Dom Processing Time: #{sprintf "%.2f", dom_processing} seconds."
+    response_time = @browser.performance.summary[:response_time].to_f / 1000
+    load_time = @browser.performance.summary[:dom_processing].to_f / 1000
+    puts "#{ENV["BROWSER"]} Repsonse Time: #{sprintf "%.2f", response_time} seconds."
+    puts "#{ENV["BROWSER"]} Load Time: #{sprintf "%.2f", load_time} seconds."
   end
 end
