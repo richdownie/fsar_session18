0c1b72aa7093d02a363545bb75b21f77e0dc1a25
Rich Downie <rich@blackboxd.com>
2019-11-28 12:01:55 -0500

    updates

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,13 +1,3 @@
-require 'selenium-webdriver'
-require 'webdrivers'
-require 'cucumber'
-require 'rspec'
-require 'pry'
-require 'httparty'
-require 'watir'
-require 'watir-performance'
-require_relative '../page_objects/widgets_index_page'
-
 Before do
   if ENV["BROWSER"]
     @browser = Watir::Browser.new ENV["BROWSER"].to_sym
